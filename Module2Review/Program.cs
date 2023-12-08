70e8650482e1735b16fbae1596bc7ef9aa25bad6
Amoriss <amoriss@protonmail.com>
2023-12-08 17:09:03 -0600

    initial commit

---
+++ Module2Review/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Module2Review;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Module2Review/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Review;
+internal class Student
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public List<double> Grades { get; set; }
+
+    public void AddGrade()
+    {
+
+    }
+}
