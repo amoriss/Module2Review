f681631dd88d2c49f545070fee04687ec9c69987
Amoriss <amoriss@protonmail.com>
2023-12-11 00:01:02 -0600

    edited student class and created objects with notes

--- Module2Review/Program.cs
+++ Module2Review/Program.cs
@@ -4,6 +4,32 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //dot notation
+        Student student1 = new Student();
+        student1.ID = 1;
+        student1.Name = "John Smith";
+        
+
+        //object initializer syntax
+        Student student2 = new Student() { ID = 2, Name = "Lisa Jones"};
+    
+
+        //custom constructor
+        Student student3 = new Student(3, "Jacob James");
+        Student student4 = new Student(4, "Bob Ashley");
+
+       
+        //adding grades
+        student1.AddGrade(96.4);
+        student1.AddGrade(78.4, 90.0, 87.3);
+        student2.AddGrade(87.9, 90.4, 97.2);
+        student3.AddGrade(70.5);
+        student4.AddGrade(55.9, 72.5);
+
+
+
+
+
+
     }
 }
--- Module2Review/Student.cs
+++ Module2Review/Student.cs
@@ -7,12 +7,25 @@ using System.Threading.Tasks;
 namespace Module2Review;
 internal class Student
 {
+    public Student()
+    {
+        
+    }
+    public Student(int id, string name)
+    {
+        ID = id;
+        Name = name;
+    }
     public int ID { get; set; }
     public string Name { get; set; }
-    public List<double> Grades { get; set; }
+    public List<double> Grades { get; set; } = new List<double>();
 
-    public void AddGrade()
+    public void AddGrade(double grade)
     {
-
+        Grades.Add(grade);
+    }
+    public void AddGrade(params double[] grades) 
+    {
+        Grades.AddRange(grades);
     }
 }
